3b243e38f47ba0f81d56d05c0790df9190f177ca
Venkata Sai Pavan Kumar Pothuri <vepo0622@netcracker.com>
2023-09-09 23:24:18 +0530

    auto

--- roboshop/ec2_launch.sh
+++ roboshop/ec2_launch.sh
@@ -1,5 +1,10 @@
 #!bin/bash
 
+if [-z $1] ; then
+echo "Enter the component"
+fi
+
+
 AMI_ID="ami-0e9fc91dd15aae68b"
 SC_ID="sg-09bbbc1d1d1aac5ee"
 Instance=t3.micro
@@ -8,11 +13,7 @@ COMPONENT=$1
 
 
 
-aws ec2 run-instances --image-id ${AMI_ID}  --instance-type ${Instance} \
+PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID}  --instance-type ${Instance} \
  --security-group-ids ${SC_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"\
-  | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'
+  | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-
-  aws ec2 run-instances --image-id ${AMI_ID}  --instance-type ${Instance} \
- --security-group-ids ${SC_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"\
-  | jq '.Instances' 
