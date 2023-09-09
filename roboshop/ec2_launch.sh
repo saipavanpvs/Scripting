b3479dae59ff31082d6fca4d408a500613ede34d
Venkata Sai Pavan Kumar Pothuri <vepo0622@netcracker.com>
2023-09-09 22:37:01 +0530

    auto

--- roboshop/ec2_launch.sh
+++ roboshop/ec2_launch.sh
@@ -3,6 +3,6 @@
 AMI_ID="ami-0c1d144c8fdd8d690"
 SC_ID="sg-09bbbc1d1d1aac5ee"
 Instance=t3.micro
-
+COMPONENT=$1
 
 aws ec2 run-instances --image-id ${AMI_ID}  --instance-type ${Instance} --security-group-ids ${SC_ID} --tag-specificationS 'ResourceType=instance,Tags=[{key=Name,Value=${COMPONENT}}]'
