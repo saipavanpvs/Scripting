7491c8a736c7a94fb3a2bfdd8becd4b73063c5a4
Venkata Sai Pavan Kumar Pothuri <vepo0622@netcracker.com>
2023-09-10 00:07:19 +0530

    auto

--- roboshop/ec2_launch.sh
+++ roboshop/ec2_launch.sh
@@ -8,8 +8,9 @@ if [ -z $1 ]  ; then
 fi 
 
 
-AMI_ID="ami-0e9fc91dd15aae68b"
-SC_ID="sg-09bbbc1d1d1aac5ee"
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7-Backup"| jq '.Images[].ImageId'|sed -e 's/"//g')
+SC_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=b55-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
+
 Instance=t3.micro
 
 
@@ -20,4 +21,7 @@ PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID}  --instance-type ${Insta
  --security-group-ids ${SC_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"\
   | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 echo ${PRIVATE_IP}
+
+
+
 
