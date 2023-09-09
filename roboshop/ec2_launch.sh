ee56bad844191b1fb0db008ee25a690c9339e984
Venkata Sai Pavan Kumar Pothuri <vepo0622@netcracker.com>
2023-09-09 22:49:19 +0530

    auto

--- roboshop/ec2_launch.sh
+++ roboshop/ec2_launch.sh
@@ -5,4 +5,4 @@ SC_ID="sg-09bbbc1d1d1aac5ee"
 Instance=t3.micro
 COMPONENT=$1
 
-aws ec2 run-instances --image-id ${AMI_ID}  --instance-type ${Instance} --security-group-ids ${SC_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}"
+aws ec2 run-instances --image-id ${AMI_ID}  --instance-type ${Instance} --security-group-ids ${SC_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
